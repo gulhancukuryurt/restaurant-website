56473ccdc832af5c974d7ae212a3a779e8ce0752
gulhancukuryurt <gcukuryurt@gmail.com>
2023-03-01 18:05:30 +0300

    menu list add

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -10,11 +10,31 @@ const Menu = () => {
               <Typography variant="h5" sx={{ color: "#9fc5dd", fontSize:"30px", textAlign: "center", mt:5 }}>
                                     Menu List
                                 </Typography>
-            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 8, m: 5 }}>
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 8, m: 5,
+             }}>
                 {
                     MenuList.map((menu) => (
 
-                        <Card sx={{ maxWidth: 400,border: '5px solid #9fc5dd'}}>
+                        <Card sx={{ maxWidth: 400,border: '5px solid #9fc5dd',
+                        
+                        "@media (max-width:1400px)": {
+                            width: 400,
+                            ml:25
+                          },
+                        "@media (max-width:900px)": {
+                            width: 600,
+                            ml:20
+                          },
+                        "@media (max-width:600px)": {
+                            width: 450,
+                            ml:3
+                          },
+                          "@media (max-width:400px)": {
+                            width: 450,
+                            ml:2
+                          },
+                          
+                        }}>
                             <CardMedia
                                 height="300px"
                                 width="300px"
@@ -42,7 +62,26 @@ const Menu = () => {
                                     
                                 </Typography>
                                 
-                                <Rating sx={{mx:15}} name="size-medium" defaultValue={3} />
+                                <Rating 
+                                sx={{
+                                    mx:15,
+                                    "@media (max-width:1030px)": {
+                                        mx: 15,
+                                      },
+                                      "@media (max-width:600px)": {
+                                          mx:15
+                                      },
+                                      "@media (max-width:500px)": {
+                                        mx:9
+                                    },
+                                      "@media (max-width:400px)": {
+                                       mx:6,
+                                      },
+                                      "@media (max-width:320px)": {
+                                        mx:3,
+                                       }, 
+                                       
+                                    }} name="size-medium" defaultValue={3} />
 
                                 
                             </CardContent>
